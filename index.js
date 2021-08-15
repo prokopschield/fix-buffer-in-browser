e5942e850c3b6a7ad1fbb63af5445dd715016f1a
Prokop <prokop.schield@protonmail.com>
2021-08-15 22:23:55 +0200

    v0.1.1 fix compatibility with svelte

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-if (!('Buffer' in (global || window))) {
-	Object.assign(global || window, require('buffer/'));
+if (typeof window === 'undefined') {
+	Object.assign(global, {
+		window: global,
+	});
 }
+
+if (typeof global === 'undefined') {
+	Object.assign(window, {
+		global: window,
+	});
+}
+
+if (!('Buffer' in window)) {
+	Object.assign(window, require('buffer/'));
+}
+
 module.exports = Buffer;
